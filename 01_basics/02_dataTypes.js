3a943c12b597e03b8165c90220c3c3b4db98d798
Sushant Shinde <98010962+Maverick341@users.noreply.github.com>
2025-10-18 18:57:53 +0530

    Add data types demonstration in 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as newer version
+
+// alert("Hello")  // won't work in nodejs (only for browser)
+
+// code readability should be high
+
+let myname = "Sushant"
+let age = 22
+let isLoggedIn = false
+let state; // undefined
+
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean =>  true/false
+// null => standalone value
+// undefined => variable is uninitialized
+// symbol
+
+// object
+
+console.log(typeof myname);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
