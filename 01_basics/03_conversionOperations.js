381e25e7c812fa3c0d301a2029c57845aefb29d4
Sushant Shinde <98010962+Maverick341@users.noreply.github.com>
2025-10-18 19:43:46 +0530

    confusing string to number conversion examples

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -3,12 +3,12 @@
 // let score = false
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" =>  33
 // "33abc" => NaN
@@ -18,7 +18,7 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true, 0 => false
 // "" => false
@@ -27,6 +27,51 @@ console.log(booleanIsLoggedIn);
 let someNum = 42
 
 let stringNumber = String(someNum)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// OPERATIONS
+
+let value = 3
+let negativeValue = -value
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let givenName = "Sushant"
+let familyName = "Shinde"
+let fullName = givenName + " " + familyName
+// console.log(fullName);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+let postfixValue = gameCounter++
+let prefixValue = ++gameCounter
+console.log(postfixValue);
+console.log(prefixValue);
+
+
+
+
+
+
+
 
